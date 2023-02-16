e0b39da1dde9c9b76ed9a3b47ab2fe12031b3b95
Tykea <Tykeaboyloy@gmail.com>
2023-02-16 17:29:27 +0700

    completed crud project wtih ruby

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -15,11 +15,39 @@ class NotesController < ApplicationController
     end
 
     def create
-        note_params = params[:note]
-        note = Note.new(title:note_params[:title],body:note_params[:body],author:note_params[:author])
-        note.save!
-        if note.save
-            redirect_to root_path
+        @note = Note.new(note_params)
+        if @note.save
+            redirect_to notes_path
+          else
+            render :new, status: :unprocessable_entity
+          end
+    end
+
+
+    def edit
+        @head = "Edit the note"
+        @note = Note.find(params[:id])
+    end
+
+    def update
+        @note = Note.find(params[:id])
+
+        if @note.update(note_params)
+            redirect_to @note
+        else
+            render :edit,status: :unprocessable_entity
         end
+    end
+
+    def destroy 
+        @note = Note.find(params[:id])
+        @note.destroy
+
+        redirect_to notes_path
+    end
+
+    private
+    def note_params
+        params.require(:note).permit(:title,:body,:author)
     end
 end
